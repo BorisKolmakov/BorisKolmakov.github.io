af28b2cbf9e1666d51469dedcdc2f9bfe2b94c79
BorisKolmakov <132357583+BorisKolmakov@users.noreply.github.com>
2023-05-19 15:19:06 +0700

    JS Read more

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,16 @@
+// service-devices__swiper
+
 const swiper = new Swiper('.swiper', {
   // slidesPerView: 11,
   slidesPerView: 1,
   spaceBetween: 20,
+  longSwipes: false,
   // Responsive breakpoints
   breakpoints: {
     // when window width is >= 320px
     320: {
       slidesPerView: 2,
-      spaceBetween: 20
+      spaceBetween: 10
     },
     // when window width is >= 480px
     480: {
@@ -30,3 +33,47 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+
+// service-devices__more
+
+const serviceDevicesMore = document.querySelector('.service-devices__more');
+const serviceDevicesBlocks = document.querySelector('.service-devices__blocks');
+
+serviceDevicesMore.onclick = function() {
+  serviceDevicesBlocks.classList.toggle('service-devices__blocks--open');
+  serviceDevicesMore.classList.toggle('service-devices__more--open');
+  if (serviceDevicesBlocks.classList.contains('service-devices__blocks--open')) {
+  serviceDevicesMore.textContent = 'Скрыть';
+  } else {
+    serviceDevicesMore.textContent = 'Показать все';
+  };
+};
+
+// service__more
+
+const serviceMore = document.querySelector('.service__more');
+const serviceParagraph = document.querySelector('.service__paragraph');
+
+serviceMore.onclick = function() {
+  serviceParagraph.classList.toggle('service__paragraph--open');
+  serviceMore.classList.toggle('service__more--open');
+  if (serviceParagraph.classList.contains('service__paragraph--open')) {
+    serviceMore.textContent = 'Свернуть';
+  }else {
+    serviceMore.textContent = 'Читать далее';
+  };
+};
+
+// asside-menu
+
+const headerBurger = document.querySelector('.header__burger');
+const asideMenuBurger = document.querySelector('.aside-menu__burger');
+const asideMenuWrap = document.getElementsByClassName('aside-menu__wrapper');
+
+headerBurger.onclick = function() {
+  asideMenuWrap [0].style.display = 'block';
+};
+
+asideMenuBurger.onclick = function() {
+  asideMenuWrap [0].style.display = 'none';
+};
